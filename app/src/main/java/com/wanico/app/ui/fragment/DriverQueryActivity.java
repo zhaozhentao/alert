26491fb024e8bfe6157c51ca6e491f2471a4fac7
zhaozhentao <344696734@qq.com>
2018-01-03 23:06:03 +0800

    轨迹查询

--- app/src/main/java/com/wanico/app/ui/fragment/DriverQueryActivity.java
+++ app/src/main/java/com/wanico/app/ui/fragment/DriverQueryActivity.java
@@ -1,5 +1,10 @@
 package com.wanico.app.ui.fragment;
 
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+
 import com.wanico.app.R;
 import com.wanico.app.ui.activity.BaseActivity;
 
@@ -7,7 +12,8 @@ import com.wanico.app.ui.activity.BaseActivity;
  * Created by zhaotao on 2018/1/1.
  */
 
-public class DriverQueryActivity extends BaseActivity {
+public class DriverQueryActivity extends BaseActivity implements View.OnClickListener {
+
     @Override
     protected int getLayoutResourceId() {
         return R.layout.driver_query_activity;
@@ -15,6 +21,32 @@ public class DriverQueryActivity extends BaseActivity {
 
     @Override
     protected void onActivityCreate() {
+        setOnClickListeners(new int[]{R.id.query}, this);
+    }
 
+    private void 条件搜索弹窗() {
+        new DialogController(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.query:
+                条件搜索弹窗();
+                break;
+        }
+    }
+
+    private class DialogController {
+
+        private DialogController(Context context) {
+            View view = LayoutInflater.from(context).inflate(R.layout.dialog_car_driver_query, null, false);
+
+            AlertDialog dialog = new AlertDialog.Builder(context)
+                .setView(view)
+                .show();
+
+            view.findViewById(R.id.query).setOnClickListener(v -> dialog.dismiss());
+        }
     }
 }
