8e56172e2dd60170dd7ffe13d5f5759ec889a83d
zhaozhentao <344696734@qq.com>
2018-01-08 22:56:40 +0800

    广告viewpager

--- app/src/main/java/com/alert/ui/fragment/ManagerFragment.java
+++ app/src/main/java/com/alert/ui/fragment/ManagerFragment.java
@@ -1,10 +1,16 @@
 package com.alert.ui.fragment;
 
 import android.content.Intent;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.rctd.platfrom.rcpingan.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by zhaotao on 2017/12/27.
@@ -20,6 +26,37 @@ public class ManagerFragment extends BaseFragment implements View.OnClickListene
     @Override
     protected void onCreateView(LayoutInflater inflater) {
         setOnClickListeners(new int[]{R.id.car_query, R.id.driver_query, R.id.add_duka, R.id.duka_manager}, this);
+
+        显示广告内容();
+    }
+
+    private void 显示广告内容() {
+        ArrayList<String> sd = new ArrayList<>();
+        sd.add("");
+        sd.add("");
+        sd.add("");
+
+        ViewPager pager = findViewById(R.id.pager);
+        Adapter adapter = new Adapter(sd);
+        pager.setAdapter(adapter);
+
+        LinearLayout dotsContainer = findViewById(R.id.dots);
+        dotsContainer.removeAllViews();
+        View[] dots = new View[adapter.getCount()];
+        for (int i = 0; i < adapter.getCount(); i++) {
+            dots[i] = getLayoutInflater().inflate(R.layout.dot, dotsContainer, true);
+            dots[i] = ((LinearLayout) dots[i]).getChildAt(i);
+            dots[i].setEnabled(i == 0);
+        }
+
+        pager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                for (int i = 0; i < dots.length; i++) {
+                    dots[i].setEnabled(i == position);
+                }
+            }
+        });
     }
 
     @Override
@@ -41,6 +78,42 @@ public class ManagerFragment extends BaseFragment implements View.OnClickListene
                 startActivity(new Intent(getActivity(), DukaManagerActivity.class));
                 break;
             }
+        }
+    }
+
+    private class Adapter extends PagerAdapter {
+
+        private View[] views;
+
+        private Adapter(ArrayList<String> ads) {
+            views = new View[ads.size()];
+
+            for (int i = 0; i < views.length; i++) {
+                views[i] = getLayoutInflater().inflate(R.layout.pager_ad, null, false);
+
+                View root = views[i];
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return views.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            container.addView(views[position]);
+            return views[position];
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
         }
     }
 }
