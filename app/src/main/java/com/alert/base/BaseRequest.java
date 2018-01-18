299170977c8941832d60f50feb4715ddf1ef6977
zhaozhentao <344696734@qq.com>
2018-01-18 23:41:37 +0800

    header

--- app/src/main/java/com/alert/base/BaseRequest.java
+++ app/src/main/java/com/alert/base/BaseRequest.java
@@ -1,13 +1,22 @@
 package com.alert.base;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
+
 import com.alert.App;
 import com.google.gson.Gson;
 import com.igexin.sdk.PushManager;
+import com.rctd.platfrom.rcpingan.BuildConfig;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.HashMap;
 
+import base.AppManager;
 import base.core.http.HttpFactory;
 import base.core.http.api.HttpListener;
 import base.core.http.request.HttpMethod;
@@ -63,6 +72,21 @@ public class BaseRequest {
         }
         request.addParam("jsondata", body);
 
+        App app = App.getInstance();
+        request.addHeader("clientId", PushManager.getInstance().getClientid(app));
+        request.addHeader("appVer", BuildConfig.VERSION_NAME);
+        request.addHeader("channelId", "channel");
+        if (ActivityCompat.checkSelfPermission(app, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+            TelephonyManager telephonyManager = (TelephonyManager) app.getSystemService(app.TELEPHONY_SERVICE);
+            request.addHeader("imei", telephonyManager.getDeviceId());
+            request.addHeader("imsi", telephonyManager.getSubscriberId());
+        }
+        request.addHeader("macId", "macId");
+        request.addHeader("sysVer", android.os.Build.VERSION.SDK);
+        request.addHeader("devType", Build.MODEL);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        AppManager.getInstance().currentActivity().getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        request.addHeader("devXy", displayMetrics.widthPixels + ":" + displayMetrics.heightPixels);
         if (StringUtils.isNotEmpty(token)) {
             request.addHeader("appToken", token);
         }
--- app/src/main/java/com/alert/ui/activity/AddDukaActivity.java
+++ app/src/main/java/com/alert/ui/activity/AddDukaActivity.java
@@ -1,6 +1,5 @@
 package com.alert.ui.activity;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.view.View;
